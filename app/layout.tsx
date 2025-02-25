0b58146b8499cb952aaa73e95aa4d84e9ce643ec
usernet2 <user.git24@gmail.com>
2025-02-26 00:28:02 +0300

    composants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ResponsiveNav from "./components/Home/Navbar/ResponsiveNav";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
